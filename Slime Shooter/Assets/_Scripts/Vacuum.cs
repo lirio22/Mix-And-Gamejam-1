829a0a548235c39c95c384c6e32e719f66267552
lirio22 <maira_sayuri@hotmail.com>
2019-10-12 01:08:38 -0300

    First Commit

---
+++ Slime Shooter/Assets/_Scripts/Vacuum.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuum : MonoBehaviour
+{
+    [SerializeField] private Effector2D effector;
+
+    private void Update()
+    {
+        Suction();
+    }
+
+    private void Suction()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            effector.enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            effector.enabled = false;
+        }
+    }
+
+    private void Reverse()
+    {
+        //Atira slime
+    }
+}
---
+++ Slime Shooter/Assets/_Scripts/VacuumRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class VacuumRotation : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    private Vector2 mousePosition;
+    private Vector2 direction;
+
+    private void Update()
+    {
+        FollowMouse();
+    }
+
+    private void FollowMouse()
+    {
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.right = direction;
+    }
+}
